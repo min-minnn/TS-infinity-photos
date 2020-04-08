2e4bd26b705934e35a9040e2e435a8c8b2a9c35f
min-minnn <alswlk0306@gmail.com>
2020-04-08 19:42:06 +0900

    Photos component rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from './components/Title';
+import Photos from './components/Photos';
 
 const App: React.FC = () => {
   return (
     <>
       <Title />
+      <Photos />
     </>
   );
 }
