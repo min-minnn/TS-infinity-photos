c664e0151e67aaa9c4fe70241969e0c280d0b7c3
min-minnn <alswlk0306@gmail.com>
2020-04-14 20:42:45 +0900

    Fix infinite scroll error

--- src/components/Photos.tsx
+++ src/components/Photos.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, RefObject } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import PhotoItems from './PhotoItems';
+import { useIntersection } from './hooks/useIntersectionObserver';
 
 export interface Photo {
   id: string,
@@ -10,9 +11,16 @@ export interface Photo {
   }
 };
 
+interface paramsProps {
+  client_id: string,
+  page: number,
+  per_page: number
+}
+
 const PhotoBlock = styled.div`
   height: 100%;
   overflow: auto;
+
   img{
     &:hover {
       filter: brightness(50%);
@@ -20,49 +28,51 @@ const PhotoBlock = styled.div`
   }
 `;
 
-const Loading = styled.h4`
-  padding: 1rem;
-`;
-
-const Error = styled.h4`
-  padding: 1rem;
+const Loading = styled.h3`
+  display: flex;
+  justify-content: center;
+  color: blue;
 `;
 
 const Photos: React.FC = () => {
   const [photos, setPhotos] = useState<Photo[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState(null);
+  const pageNumber: React.MutableRefObject<number> = useRef<number>(0);
+  const ref: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
 
-  const fetchMorePhotos = async() => {
-    try {
-      setLoading(true);
-      await axios.get('https://api.unsplash.com/photos/random', {
-        params: {
-          client_id: 'XjG_79dJXuU7Zx5TRHZBBmSTuqTdcAs_LtyesusCJNU',
-          count: 30
-        }
-      })
-      .then((response) => {
-        setPhotos(photos.concat(response.data));
-      }); 
-    } catch (e) {
-      setError(e);
-    }
-    setLoading(false);
-  };
+  const intersection = useIntersection(ref, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1.0,
+  });
+
+  const LoadPhotos = async() => {
+    const response = await axios.get('https://api.unsplash.com/photos', {
+      params: {
+        client_id: '8ef4aJi0nfuZjm6Ulw5AxeIL0HKNJRFQNAzpL8jQ0dQ',
+        page: pageNumber.current,
+        per_page: 10
+      }})
+    return response;
+  }
 
   useEffect(() => {
-    fetchMorePhotos();
+    if (intersection && intersection.isIntersecting) {
+      setTimeout(async() => {
+        const response = await LoadPhotos();
+        setPhotos([...photos, ...response.data]);
+      }, )
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [photos])
+  }, [intersection])
 
   return(
     <PhotoBlock>
       {photos.map(photo => (
         <PhotoItems key={photo.id} photos={photo} />
       ))}
-      {loading && <Loading>Loading...</Loading>}
-      {error && <Error>error</Error>}
+      <div ref={ref}>
+        {intersection ? <Loading>loading</Loading> : null}
+      </div>
     </PhotoBlock>
   )
 }
